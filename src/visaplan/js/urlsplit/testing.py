c357b5a14f9bc4553010205eede718cb51904226
Tobias Herp <tobias.herp@stein.de>
2019-07-05 09:43:04 +0200

    Avoid ImportWarnings; no Acceptancetesting yet
    In our Plone 4.3 setup, wo could make the AcceptanceTesting part work.
    
    With zope.testing 4.8+, there are lots of ImportWarnings during the core
    packages import. Wo don't want to ignore all DeprecationWarnings (we
    hope to get along with this filter for now) but the most annoying ones.

--- src/visaplan/js/urlsplit/testing.py
+++ src/visaplan/js/urlsplit/testing.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
-from plone.app.testing import applyProfile
-from plone.app.testing import FunctionalTesting
-from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
-from plone.app.testing import PloneSandboxLayer
-from plone.testing import z2
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', ImportWarning)
+
+    # from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+    from plone.app.testing import applyProfile
+    from plone.app.testing import FunctionalTesting
+    from plone.app.testing import IntegrationTesting
+    from plone.app.testing import PLONE_FIXTURE
+    from plone.app.testing import PloneSandboxLayer
+    from plone.testing import z2
 
 import visaplan.js.urlsplit
 
@@ -41,11 +45,11 @@ VISAPLAN_JS_URLSPLIT_FUNCTIONAL_TESTING = FunctionalTesting(
 )
 
 
-VISAPLAN_JS_URLSPLIT_ACCEPTANCE_TESTING = FunctionalTesting(
-    bases=(
-        VISAPLAN_JS_URLSPLIT_FIXTURE,
-        REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE,
-    ),
-    name='VisaplanJsUrlsplitLayer:AcceptanceTesting',
-)
+# VISAPLAN_JS_URLSPLIT_ACCEPTANCE_TESTING = FunctionalTesting(
+#     bases=(
+#         VISAPLAN_JS_URLSPLIT_FIXTURE,
+#         REMOTE_LIBRARY_BUNDLE_FIXTURE,
+#         z2.ZSERVER_FIXTURE,
+#     ),
+#     name='VisaplanJsUrlsplitLayer:AcceptanceTesting',
+# )
